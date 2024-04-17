16c77b2f8a7010a57efd996627de3c99c051a08f
Joey <82052967+jlovrek1@users.noreply.github.com>
2024-04-16 22:04:09 -0400

    Add files via upload

---
+++ if-statements.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter to your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age >= 18)
+{
+    Console.WriteLine("You are now signed up!");
+}
+else if ( age < 0 )
+{
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("You must be 21+ to sign up");
+}
